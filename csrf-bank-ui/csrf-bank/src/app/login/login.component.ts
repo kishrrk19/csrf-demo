7240099eeafd5e94897d7f40083fe6162ed8e439
Rika Kishiro <rika.kishiro@gmail.com>
2025-07-11 12:42:42 +0200

    Test cedar

--- csrf-bank-ui/csrf-bank/src/app/login/login.component.ts
+++ csrf-bank-ui/csrf-bank/src/app/login/login.component.ts
@@ -34,7 +34,9 @@ export class LoginComponent implements OnInit {
 
     if (this.loginForm.valid) {
       const formData = this.loginForm.value;
-      this.http.post(`${this.baseUrl}/bank/login`, formData).subscribe({
+      this.http.post(`${this.baseUrl}/projects/all`, formData, {
+        withCredentials: true
+      }).subscribe({
         next: () => {
           this.router.navigate(['/home']);
         },
